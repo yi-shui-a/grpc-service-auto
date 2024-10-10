d5df69eebcad44b105c16412178ba622044f119b
yishui <1480685741@qq.com>
2024-10-10 14:13:51 +0800

    10-10 reset

--- new_atom_service_mbsb.c
+++ new_atom_service_mbsb.c
@@ -39,27 +39,3 @@ int atomic_service_fun_task_D(atomic_service_mbsb_task_D_Request_st *request, at
         reply->sum_result = (request->number1) / (request->number2);
     return ATOMIC_SERVICE_SUCCESS;
 }
-
-
-// void atomic_service_mbsb_func_A(atomic_service_mbsb_task_A_Request_st *request, atomic_service_mbsb_task_A_Reply_st *reply){
-// 	// Function implementation TODO 
-//     int ret_type = atomic_service_fun_task_A(request, reply);
-//     reply->return_type = ret_type;
-// }
-// void atomic_service_mbsb_func_B(atomic_service_mbsb_task_B_Request_st *request, atomic_service_mbsb_task_B_Reply_st *reply){
-// 	// Function implementation TODO 
-//     int ret_type = atomic_service_fun_task_B(request, reply);
-//     reply->return_type = ret_type;
-// }
-// void atomic_service_mbsb_func_C(atomic_service_mbsb_task_C_Request_st *request, atomic_service_mbsb_task_C_Reply_st *reply){
-// 	// Function implementation TODO 
-//     int ret_type = atomic_service_fun_task_C(request, reply);
-//     reply->return_type = ret_type;
-// }
-// void atomic_service_mbsb_func_D(atomic_service_mbsb_task_D_Request_st *request, atomic_service_mbsb_task_D_Reply_st *reply){
-// 	// Function implementation TODO 
-//     int ret_type = atomic_service_fun_task_D(request, reply);
-//     reply->return_type = ret_type;
-// }
-
-
