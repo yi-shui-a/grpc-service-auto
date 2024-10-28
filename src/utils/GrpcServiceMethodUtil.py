b95bfe51e56bde62db3b60794d78f1ec0fe670fa
yishui <1480685741@qq.com>
2024-10-28 16:51:30 +0800

    修复proto生成时的数据类型出错问题

--- src/utils/GrpcServiceMethodUtil.py
+++ src/utils/GrpcServiceMethodUtil.py
@@ -7,6 +7,7 @@ import ServiceMethod
 import OperatingSystem
 import AtomService
 import Message
+from config.types import cpp_types
 
 class GrpcServiceMethodUtil:
     def __init__(self):
@@ -22,6 +23,7 @@ class GrpcServiceMethodUtil:
         self._service_methods = service._service_methods
         self._messages = service._messages
         self._generate_info()
+        self._type_convert()
         
         
     def _check_situation(self)->bool:
@@ -32,16 +34,27 @@ class GrpcServiceMethodUtil:
     def _generate_info(self):
         self.__service_name_proto = self.__service_name + "_service"
         self.__service_name_package = self.__service_name +"_package"
+        
+    def _type_convert(self):
+        # 修改数据类型
+        # 删除std::
+        for message in self._messages:
+            for field in message._fields:
+                    field._type = cpp_types.get(field._type,field._type)
+                    if field._type.count("std::") > 0:
+                        field._type = field._type.replace("std::", "")
+                    
+        
+            
 
     def generateProtoFile(self):
         # TODO: Read the input file and extract the necessary information
         # Example: Read from inputFilrName and write to outputFilrName
         # 定义模板
         proto_template = Template(open(f"{os.path.dirname(os.path.abspath(__file__))}/../../Jinja2/proto_template.j2").read())
+        
+        
         res_str = proto_template.render(service_name_package = self.__service_name_package, service_name_proto = self.__service_name_proto, messages = self._messages, methods = self._service_methods)
-        
-        # 此处需要加一个类型转换，通过加载一个dict检索所有需要转换的类型，然后转换数据类型
-        
         
         # 将res_str写入框架内的cpp文件中，同名不同路径
         with open(f"{os.path.dirname(os.path.abspath(__file__))}/../../protos/{self.__service_name}.proto", 'w') as file:
--- test_module.py
+++ test_module.py
@@ -1,3 +1,5 @@
+import time
+
 from src.utils.ServiceUtil import ServiceUtil
 from src.utils.GrpcServiceMethodUtil import GrpcServiceMethodUtil
 
@@ -5,15 +7,20 @@ if __name__ == '__main__':
 
     serviceUtilsA = ServiceUtil()
     serviceUtilsB = ServiceUtil()
-
+    # start = time.time()
     serviceUtilsA.parseHpp("/root/grpc-generate-server/input_inc/atomic_service_mbsb.h")
-    serviceUtilsA.parseCpp("/root/grpc-generate-server/input_src/atomic_service_mbsb.cpp")
     serviceUtilsB.parseHpp("/root/grpc-generate-server/input_inc/atomic_service_sf.h")
+    # end1 = time.time()
+    serviceUtilsA.parseCpp("/root/grpc-generate-server/input_src/atomic_service_mbsb.cpp")
     serviceUtilsB.parseCpp("/root/grpc-generate-server/input_src/atomic_service_sf.c")
+    # end2 = time.time()
     
-    methodUtilsA = GrpcServiceMethodUtil()
-    methodUtilsB = GrpcServiceMethodUtil()
-    methodUtilsA.set_service_method_util(serviceUtilsA._service)
-    methodUtilsB.set_service_method_util(serviceUtilsB._service)
-    methodUtilsA.generateProtoFile()
-    methodUtilsB.generateProtoFile()
+    # print(".h parse time: ",(end1-start)*1000/2)
+    # print(".cpp parse time: ",(end2-end1)*1000/2)
+    
+    # methodUtilsA = GrpcServiceMethodUtil()
+    # methodUtilsB = GrpcServiceMethodUtil()
+    # methodUtilsA.set_service_method_util(serviceUtilsA._service)
+    # methodUtilsB.set_service_method_util(serviceUtilsB._service)
+    # methodUtilsA.generateProtoFile()
+    # methodUtilsB.generateProtoFile()
