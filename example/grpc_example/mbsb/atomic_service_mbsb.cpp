1a17f9c0a90cf8dff0398b6d90a2eb101f18bdab
yishui <1480685741@qq.com>
2025-04-24 14:40:42 +0000

    refact: 修改hpp解析代码和grpc测试文件名

---
+++ example/grpc_example/mbsb/atomic_service_mbsb.cpp
@@ -0,0 +1,18 @@
+#include "../atom_inc/atom_service_mbsb.h"
+
+int atomic_service_fun_task_A(atomic_service_mbsb_task_A_Request_st *request, atomic_service_mbsb_task_A_Reply_st *reply){
+	// TODO: add your code here
+    // request->number1 = 123;
+    // request->number2 = 456;
+    reply->sum_result = request->number1 + request->number2;
+    
+    return ATOMIC_SERVICE_SUCCESS;
+}
+
+int atomic_service_fun_task_B(atomic_service_mbsb_task_B_Request_st *request, atomic_service_mbsb_task_B_Reply_st *reply){
+	// TODO: add your code here
+    request->number1 = 123;
+    request->number2 = 456;
+    reply->sub_result = request->number1 - request->number2;
+    return ATOMIC_SERVICE_SUCCESS;
+}
--- test/test_grpc_module.py
+++ test/test_grpc_module.py
@@ -6,33 +6,36 @@ import os
 # 将搜索目录改为上一级目录
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 
-from src.scripts.util.AtomServiceUtil import AtomServiceUtil
-from src.scripts.util.GrpcMethodUtil import GrpcMethodUtil
-from src.scripts.util.ServerUtil import ServerUtil
-from scripts.entity.Server import Server
-from src.scripts.util.Util import Util
-from src.scripts.util.DDSUtil import DDSUtil
+# from src.scripts.util.AtomServiceUtil import AtomServiceUtil
+# from src.scripts.util.GrpcMethodUtil import GrpcMethodUtil
+# from src.scripts.util.ServerUtil import ServerUtil
+# from scripts.entity.Server import Server
+# from src.scripts.util.Util import Util
+# from src.scripts.util.DDSUtil import DDSUtil
 
+from src.scripts.util import *
+from src.scripts.entity import *
 
-def test_service():
+if __name__ == "__main__":
+
     """
     atomic_service相关的函数
     """
     # 解析h文件，生成service对象
     serviceA = AtomServiceUtil.parseHpp(
-        f"{os.path.dirname(os.path.abspath(__file__))}/../example/mbsb/atomic_service_mbsb.h"
+        f"{os.path.dirname(os.path.abspath(__file__))}/../example/grpc_example/mbsb/atomic_service_mbsb.h"
     )
     serviceB = AtomServiceUtil.parseHpp(
-        f"{os.path.dirname(os.path.abspath(__file__))}/../example/sf/atomic_service_sf.h"
+        f"{os.path.dirname(os.path.abspath(__file__))}/../example/grpc_example/sf/atomic_service_sf.h"
     )
     # 解析 cpp 文件
     AtomServiceUtil.parseCpp(
         serviceA,
-        f"{os.path.dirname(os.path.abspath(__file__))}/../example/mbsb/atomic_service_mbsb.cpp",
+        f"{os.path.dirname(os.path.abspath(__file__))}/../example/grpc_example/mbsb/atomic_service_mbsb.cpp",
     )
     AtomServiceUtil.parseCpp(
         serviceB,
-        f"{os.path.dirname(os.path.abspath(__file__))}/../example/sf/atomic_service_sf.c",
+        f"{os.path.dirname(os.path.abspath(__file__))}/../example/grpc_example/sf/atomic_service_sf.c",
     )
 
     # 生成微服务编译的cmake
@@ -73,6 +76,19 @@ def test_service():
     GrpcMethodUtil.add_info_to_json(serviceA)
     GrpcMethodUtil.add_info_to_json(serviceB)
 
+    """
+    server相关函数
+    """
+    # 实例化一个server
+    serverB = Server()
+    with open(
+        f"{os.path.dirname(os.path.abspath(__file__))}/user_info.json", "r"
+    ) as file:
+        user_info = json.loads(file.read())
+    serverB.set_info_from_user(user_info)
+    ServerUtil.saveServerJson(serverB)
 
-if __name__ == "__main__":
-    test_service()
+    # SyncServer生成编译相关函数
+    ServerUtil.generateSyncServer(serverB)
+    ServerUtil.generateSyncServerCMakeLists(serverB)
+    ServerUtil.compileSyncServer(serverB)
